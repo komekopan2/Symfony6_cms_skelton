ffde1818e7e4c4adb8c7b4637008b0d8dcfd2f2b
komekopan2 <yoo.shimizu888@gmail.com>
2023-07-13 15:43:55 +0900

    管理画面一覧ページのロジック

--- src/Controller/Admin/TopicsController.php
+++ src/Controller/Admin/TopicsController.php
@@ -5,14 +5,17 @@ namespace App\Controller\Admin;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\Topics\PostRepository;
 
 #[Route("/admin/topics")]
 class TopicsController extends AbstractController
 {
     #[Route("/", name: "admin_topics_index", methods: ["GET"])]
-    public function index(): Response
+    public function index(PostRepository $postRepository): Response
     {
-        return $this->render("admin/topics/index.html.twig", []);
+        return $this->render("admin/topics/index.html.twig", [
+            "posts" => $postRepository->findBy([], ["postAt" => "desc"])
+        ]);
     }
     #[Route("/add", name: "admin_topics_add", methods: ["GET"])]
     public function add(): Response
